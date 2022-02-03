253a735235c028ef41310dbefb01028f4c984137
Fazlay rabbi <rfazlay21@gmail.com>
2022-02-04 01:46:14 +0600

    bug fixes

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const os = require("os-utils");
 const yahooFinance = require("yahoo-finance2").default;
-const app = express();
 
-const httpServer = createServer(app);
+const app = express();
+app.use(cors());
+
+const httpServer = createServer();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
@@ -16,23 +19,42 @@ const io = new Server(httpServer, {
   transports: ["websocket", "polling"],
 });
 
-io.on("connection", (socket) => {
+io.on("connection", (client) => {
   setInterval(() => {
     async function yahoApi() {
       const quote = await yahooFinance.quote("BTC-USD");
       const { regularMarketPrice } = quote;
-
-      socket.emit("symbol", {
+      console.log(regularMarketPrice);
+      client.emit("symbol", {
         name: "BTC-USD",
         value: regularMarketPrice,
       });
-
-      console.log(regularMarketPrice);
     }
     yahoApi();
   }, 1000);
 });
 
-app.listen(port, (req, res) => {
+// io.on("connection", (socket) => {
+//   setInterval(() => {
+//     os.cpuUsage((cpuPercent) => {
+//       console.log(cpuPercent);
+//       socket.emit("cpu", cpuPercent);
+//     });
+//     // async function yahoApi() {
+//     //   const quote = await yahooFinance.quote("BTC-USD");
+//     //   const { regularMarketPrice } = quote;
+//     //   console.log(regularMarketPrice);
+//     //   socket.emit("symbol", {
+//     //     name: "BTC-USD",
+//     //     value: regularMarketPrice,
+//     //   });
+//     // }
+//     // yahoApi();
+//   }, 1000);
+// });
+
+httpServer.listen(port, (req, res) => {
   console.log("Running the Port", port);
 });
+
+// httpServer.listen(3000);
