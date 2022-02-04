fa7b8d1dd50b9325ab5eb4bcb4f7183ae06e4ee1
Fazlay rabbi <rfazlay21@gmail.com>
2022-02-05 02:10:54 +0600

    good

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const os = require("os-utils");
-const yahooFinance = require("yahoo-finance2").default;
+const tradinglive = require("./tradinglive");
+// const yahooFinance = require("yahoo-finance2").default;
 
 const app = express();
 app.use(cors());
@@ -18,40 +18,7 @@ app.get("/", (req, res) => {
 const io = new Server(httpServer, {
   transports: ["websocket", "polling"],
 });
-
-io.on("connection", (client) => {
-  setInterval(() => {
-    async function yahoApi() {
-      const quote = await yahooFinance.quote("BTC-USD");
-      const { regularMarketPrice } = quote;
-      console.log(regularMarketPrice);
-      client.emit("symbol", {
-        name: "BTC-USD",
-        value: regularMarketPrice,
-      });
-    }
-    yahoApi();
-  }, 1000);
-});
-
-// io.on("connection", (socket) => {
-//   setInterval(() => {
-//     os.cpuUsage((cpuPercent) => {
-//       console.log(cpuPercent);
-//       socket.emit("cpu", cpuPercent);
-//     });
-//     // async function yahoApi() {
-//     //   const quote = await yahooFinance.quote("BTC-USD");
-//     //   const { regularMarketPrice } = quote;
-//     //   console.log(regularMarketPrice);
-//     //   socket.emit("symbol", {
-//     //     name: "BTC-USD",
-//     //     value: regularMarketPrice,
-//     //   });
-//     // }
-//     // yahoApi();
-//   }, 1000);
-// });
+require("./tradinglive")(io);
 
 httpServer.listen(port, (req, res) => {
   console.log("Running the Port", port);
---
+++ tradinglive.js
@@ -0,0 +1,63 @@
+const yahooFinance = require('yahoo-finance2').default;
+
+module.exports = function (io) {
+  io.on('connection', (client) => {
+    let updatedMarketPrice = {};
+    setInterval(() => {
+      const symbols = [
+        'NIFTY50.NS',
+        'MARUTI.NS',
+        'AXISBANK.NS',
+        'SBIN.NS',
+        'INDUSINDBK.NS',
+        'UPL.NS',
+      ];
+      async function yahoApi() {
+        const quote = await yahooFinance.quote(symbols);
+        quote.map((element) =>
+          // console.log(element.symbol, element.regularMarketPrice)
+
+          Object.assign(updatedMarketPrice, {
+            [element.symbol.slice(0, -3)]: element.regularMarketPrice,
+          })
+        );
+        console.log(updatedMarketPrice);
+        client.emit('symbol', updatedMarketPrice);
+      }
+
+      yahoApi();
+    }, 1000);
+  });
+};
+
+// const express = require("express");
+// const { createServer } = require("http");
+// const { Server } = require("socket.io");
+// const app = express();
+
+// // const yahooFinance = require("yahoo-finance2").default;
+
+// const httpServer = createServer();
+
+// // require("./tradinglive")();
+
+// const design = () => {
+//   const io = new Server(httpServer, {
+//     transports: ["websocket", "polling"],
+//   });
+
+//   io.on("connection", (client) => {
+//     console.log("this is mofule");
+//     setInterval(() => {
+//       const regularMarketPrices = tradinglive.habi();
+//       console.log(regularMarketPrices);
+//       client.emit("symbol", {
+//         name: "BTC-USD",
+//         value: regularMarketPrices,
+//       });
+//     }, 1000);
+//   });
+// };
+
+// module.exports.design = design;
+// // httpServer.listen(3000);
