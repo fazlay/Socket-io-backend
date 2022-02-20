e3dcbd2bdf795bb834e3c1b3d4317d4ef7665f77
Fazlay rabbi <rfazlay21@gmail.com>
2022-02-21 00:09:13 +0600

    git added

--- tradinglive.js
+++ tradinglive.js
@@ -12,42 +12,10 @@ module.exports = function (io) {
         updatedMarketPrice.push(quote.regularMarketPrice);
 
         console.log(updatedMarketPrice);
-        client.emit('symbol', updatedMarketPrice);
+        client.emit('symbol', quote);
       }
 
       yahoApi();
     }, 1000);
   });
 };
-
-// const express = require("express");
-// const { createServer } = require("http");
-// const { Server } = require("socket.io");
-// const app = express();
-
-// // const yahooFinance = require("yahoo-finance2").default;
-
-// const httpServer = createServer();
-
-// // require("./tradinglive")();
-
-// const design = () => {
-//   const io = new Server(httpServer, {
-//     transports: ["websocket", "polling"],
-//   });
-
-//   io.on("connection", (client) => {
-//     console.log("this is mofule");
-//     setInterval(() => {
-//       const regularMarketPrices = tradinglive.habi();
-//       console.log(regularMarketPrices);
-//       client.emit("symbol", {
-//         name: "BTC-USD",
-//         value: regularMarketPrices,
-//       });
-//     }, 1000);
-//   });
-// };
-
-// module.exports.design = design;
-// // httpServer.listen(3000);
