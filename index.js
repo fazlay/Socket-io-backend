c0000ad01a8dfe4d928be4f45b773bbc1b57f6f7
Fazlay rabbi <rfazlay21@gmail.com>
2022-02-22 01:42:57 +0600

    final

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-const tradinglive = require("./tradinglive");
+const express = require('express');
+const cors = require('cors');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const tradinglive = require('./tradinglive');
 // const yahooFinance = require("yahoo-finance2").default;
 
 const app = express();
@@ -11,17 +11,17 @@ app.use(cors());
 const httpServer = createServer();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World! for socket IO");
+app.get('/', (req, res) => {
+  res.send('Hello World! for socket IO');
 });
 
 const io = new Server(httpServer, {
-  transports: ["websocket", "polling"],
+  transports: ['websocket', 'polling'],
 });
-require("./tradinglive")(io);
+require('./tradinglive')(io);
 
 httpServer.listen(port, (req, res) => {
-  console.log("Running the Port", port);
+  console.log('Running the Port', port);
 });
 
 // httpServer.listen(3000);
--- tradinglive.js
+++ tradinglive.js
@@ -7,15 +7,26 @@ module.exports = function (io) {
       const symbols = 'BTC-USD';
 
       async function yahoApi() {
-        const quote = await yahooFinance.quote(symbols);
+        try {
+          const quote = await yahooFinance.quote(symbols);
 
-        // updatedMarketPrice.push(quote.regularMarketPrice);
+          if (updatedMarketPrice.length > 10) {
+            console.log(updatedMarketPrice.length);
+            updatedMarketPrice.shift();
+          }
+          updatedMarketPrice.push({
+            name: new Date().getSeconds(),
+            uv: quote.regularMarketPrice,
+          });
 
-        console.log(quote.regularMarketPrice);
-        client.emit('symbol', quote);
+          console.log(updatedMarketPrice);
+          client.emit('symbol', updatedMarketPrice);
+        } catch (error) {
+          console.log(error.message);
+        }
       }
 
       yahoApi();
-    }, 5000);
+    }, 20000);
   });
 };
