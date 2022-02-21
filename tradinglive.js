7f514a4155dfeea45506a6ae67f2c5bd28191ad0
Fazlay rabbi <rfazlay21@gmail.com>
2022-02-21 23:18:35 +0600

    refactor

--- tradinglive.js
+++ tradinglive.js
@@ -11,7 +11,7 @@ module.exports = function (io) {
 
         // updatedMarketPrice.push(quote.regularMarketPrice);
 
-        console.log(updatedMarketPrice);
+        console.log(quote.updatedMarketPrice);
         client.emit('symbol', quote);
       }
 
